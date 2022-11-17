6efacda9b3a78741780d177411b0590b29ec40d9
RowDog <mrdiamonddirt@gmail.com>
2022-11-17 16:08:06 +0000

    installed node

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 
-
 app.use("/static", express.static("public"));
 app.use("/another", express.static("public/another.html"));
 app.use("/home", express.static("public/home.html"));
